7341c1591df4ddd478274b262ebae154d433fa0a
JS-hong <lielty9371@gmail.com>
2023-09-15 11:32:01 +0900

    0915

--- test7.java
+++ test7.java
@@ -1,6 +1,6 @@
 //주차 요금 계산! not
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 public class test7 {
 	public static void main(String[] args) {
 		int[] fees = {180, 5000, 10, 600};
@@ -11,14 +11,40 @@ public class test7 {
 		List<List<String>> str = new ArrayList<List<String>>();
 		
 		for(String park : records) {
+			List<String> row = new ArrayList<>();
+
+			String[] elements = park.split(" ");
+			String[] timeElements = elements[0].split(":");//시/분 나누기
+
+			for (String element : timeElements) {
+				row.add(element);
+			}
+
+			for(int i=1;i<elements.length;i++){
+				row.add(elements[i]);
+			}
+
+			str.add(row);
 			//총 분할해야 할 데이터 
 			//1.시간 2.분 3.차번호 4.in out 유무
+		}//clear
+
+		//요금 게산용 Map
+		Map<String,Integer> parkMap = new HashMap<>();//차번호 요금 저장
+
+
+		for(int j=0;j<str.size();j++){//데이터 수 만큼 반복
+			if (parkMap.get(str.get(j).get(2)) != null) {
+				if(str.get(j).get(3) == "OUT"){
+
+				}
+				else{
+					
+				}
+			} else {
+				parkMap.put(str.get(j).get(2),fees[1]);
+			}
 		}
-		//일단 2가지 case 존재 기본시간이 남아 있을 경우 
-		//기존 시간을 다 소모하고 추가시간을 적용 시켜야 하는 경우
-		
-		//일단 기본시간 case 작성
-		//기본시간 IN OUT
-		//추후에 계산할때 MAP 사용해서 key value 형태로 삽입 및 수정
+		System.out.println(parkMap);
 	}    
 }
