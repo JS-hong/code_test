5cb8ebadb434b3bf2f987ca9f09dfb363c4bdf19
JS-hong <lielty9371@gmail.com>
2023-09-22 10:41:55 +0900

    0922

--- test14.java
+++ test14.java
@@ -1,16 +1,33 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class test14 {
-    //거스름돈 최대공약수 최소공배수 쪽 인듯?
+    //거스름돈
     public static void main(String[] args) {
         int n = 5;//잔금
         int[] money = {1,2,5};//돈    
-        Set<List<Integer>> mSet = new HashSet<>();//중복 제거용 Hashset
-
-        for(){//그냥 money안에 있는 숫
-
+        Set<List<Integer>> mSet = new HashSet<>(); // 중복 제거용 HashSet
+        calculateChange(money, n, new ArrayList<>(), mSet);//재귀함수?
+        for (List<Integer> change : mSet) {
+            System.out.println(change);
         }
+    }
+    private static void calculateChange(int[] money, int remainingAmount,
+            List<Integer> currentChange, Set<List<Integer>> changeSet) {
+            //결국 결과값을 mset에 넣고 반복문 돌리는게 핵심일듯
+            if (remainingAmount == 0) {
+                List<Integer> sortedChange = new ArrayList<>(currentChange);
+                sortedChange.sort(null); // 리스트 정렬
+                changeSet.add(sortedChange);
+                return;
+            }
+    
+            if (remainingAmount < 0) {
+                return;
+            }
+            for (int i = 0; i < money.length; i++) {
+                currentChange.add(money[i]);
+                calculateChange(money, remainingAmount - money[i], currentChange, changeSet);
+                currentChange.remove(currentChange.size() - 1);
+            }
     }
 }
